bdbe97703d05c312dd2b718030299fcb1c0a7ef6
newKijitora <katsu.sbkmmno.1974@gmail.com>
2020-08-13 21:27:33 +0900

    ファイル追加

---
+++ command/command-box/command-menu/member-selecter/member-name/item-list/item-list-context.js
@@ -0,0 +1,113 @@
+// ------------------------------------------------------------------
+// アイテムリストのコンテキスト
+// ------------------------------------------------------------------
+
+class ItemListContext extends KeyManageContext {
+  
+  // コンストラクタ
+  constructor(memberName) { super(memberName.memberSelecter.commandMenu.commandBox.town);
+    // メンバーネーム
+    this.memberName = memberName;
+
+    // 一コマのサイズと文字サイズ
+    this.squareSize = memberName.squareSize;
+    this.textSize = memberName.textSize;
+
+    // キー
+    this.openKey = memberName.memberSelecter.openKey;
+    this.closeKey = memberName.memberSelecter.closeKey;
+
+    // コマンドボックスの左上位置
+    this.itemListPosition = new Position(6, -2);
+
+    // メニューの名前
+    this.title = this.memberName.menuName;
+
+    // メンバーセレクターの背景色
+    this.backgroundColor = this.memberName.memberSelecter.backgroundColor;
+
+    // 初期ステータス
+    this.viewState = "closed";
+
+    // どうぐのリスト
+    for (let i = 0; i < this.memberName.memberSelecter.memberCharacters.length; i++) {
+      if (this.memberName.memberSelecter.memberCharacters[i].name == this.title) {
+        this.items = this.memberName.memberSelecter.memberCharacters[i].items;
+      }
+    }
+
+    // コマンドのメニュー（冒険のパーティの名前で生成）
+    this.commandMenus = new Array(this.items.length);
+
+    for (let i = 0; i < this.commandMenus.length; i++) {
+      this.commandMenus[i] = new Array(1);
+      for (let j = 0; j < this.commandMenus[i].length; j++) {
+        let isSelected = false;
+        if (i == 0) {
+          isSelected = true;
+        }
+
+        this.commandMenus[i][j] = new GameCommand("", this.items[i].name, isSelected, false);
+      }
+    }
+
+    this.currentCommandMenu = null;
+
+    // コマンドフレームのサイズ
+    this.commandBoxRows = this.items.length + 1;
+    this.commandBoxColumns = 6; // 変数を検討
+
+    //this.memberStatusController = new MemberStatusContext(this);
+    this.isChildOpened = false;
+
+    // コマンドメニューのコンテキストを生成する
+    this.memberNameContexts = new Array(this.commandMenus.length);
+
+    for (let i = 0; i < this.memberNameContexts.length; i++) {
+      this.memberNameContexts[i] = new Array(this.commandMenus[i].length);
+      for (let j = 0; j < this.memberNameContexts[i].length; j++) {
+        this.memberNameContexts[i][j] = new ItemNameContext(this, this.commandMenus[i][j], new Position(j, i));
+        
+        if (this.commandMenus[i][j].isSelected) {
+          this.currentCommandMenu = this.memberNameContexts[i][j];
+        }
+      }
+    }
+  }
+
+  // オープンできるかどうか
+  get canOpen() {
+    if (this.viewState == "opened" || this.memberName.memberSelecter.viewState != "opened" || !this.memberName.isSelected) {
+      return false;
+    }
+    return true;
+  }
+
+  // クローズできるかどうか
+  get canClose() {
+    
+    if (this.viewState == "closed" || this.isChildOpened) {
+      this.isChildOpened = false;
+      return false;
+    }
+    return true;
+  }
+
+  get canSelectionChange() {
+    // ビュー状態がclosedであればカット
+    if (this.viewState == "closed") {
+      return false;
+    }
+    
+    if (this.isChildOpened) {
+      return false;
+    }
+
+    // テキストエリアのビュー状態がopenedであればカット
+    if (this.memberName.memberSelecter.commandMenu.commandBox.textAreaContext.viewState == "opened") {
+      return false;
+    }
+
+    return true;
+  }
+}
---
+++ command/command-box/command-menu/member-selecter/member-name/item-list/item-name/item-name-view.js
@@ -0,0 +1,88 @@
+// コマンドメニューのビュー
+class ItemNameView extends CommandBoxViewBase {
+
+  // コンストラクタ
+  constructor(context, commandText, frameCanvases, charCanvases) { super(context);
+    // コンテキスト
+    this.context = context;
+
+    // フレームキャンバスと文字キャンバス
+    this.frameCanvases = frameCanvases;
+    this.charCanvases = charCanvases;
+
+    // このコマンドメニューのテキスト
+    this.commandTitle = commandText;
+
+    // HTML要素
+    this.commandMenuDOM = null;
+    this.commandPointer = null;
+    this.memberSelecter = null;
+
+    // HTML要素の生成
+    this.assemblingElements();
+  }
+  
+  // HTML要素の生成
+  assemblingElements() {
+    // コマンドメニューのコンテナ
+    const commandMenu = document.createElement("div");
+    commandMenu.style.display = "flex";
+
+    // コマンドポインター要素
+    const commandPointer = document.createElement("canvas");
+    commandPointer.width = 32;
+    commandPointer.height = 32;
+
+    // コマンドメニュー要素
+    const commandText = document.createElement("canvas");
+    commandText.width = 128; // 8文字分
+    commandText.height = 32;
+
+    // コマンドメニューのコンテナにポインターとメニューを格納
+    commandMenu.appendChild(commandPointer);
+    commandMenu.appendChild(commandText);
+
+    // 参照を保存
+    this.commandMenuDOM = commandMenu;
+    this.commandPointer = commandPointer;
+    this.commandText = commandText;
+
+    // メンバーステータスの生成
+    if (this.context.memberStatusContext) {
+      const memberStatus = new MemberStatusView(this.context.memberStatusContext, this.frameCanvases, this.charCanvases);
+      this.commandMenuDOM.appendChild(memberStatus.memberStatusDOM);
+    }
+
+    if (this.context.toolListContext) {
+      const toolList = new ToolListView(this.context.toolListContext, this.frameCanvases, this.charCanvases);
+      this.commandMenuDOM.appendChild(toolList.memberSelecter);
+    }
+  }
+
+  // コマンドメニューの文字を初期化する
+  initialize(textures) {
+    // コマンドのタイトルを描画する
+    const commandTitleContext = this.commandText.getContext("2d");
+    for (let i = 0; i < this.commandTitle.length; i++) {
+      commandTitleContext.drawImage(textures[this.commandTitle[i]], i * 16, 0);
+    }
+
+    // 自身が選択されていればコマンドポインターを表示する
+    if (this.context.isSelected) {
+      const commandPointerContext = this.commandPointer.getContext("2d");
+      commandPointerContext.drawImage(textures["commandPointer"], 16, 0);
+    }
+  }
+
+  // コマンドポインターを表示
+  showCommandPointer(textures) {
+    const context = this.commandPointer.getContext("2d");
+    context.drawImage(textures["commandPointer"], 16, 0);
+  }
+
+  // コマンドポインターを非表示
+  hideCommandPointer() {
+    const context = this.commandPointer.getContext("2d");
+    context.clearRect(0, 0, 32, 32);
+  }
+}
